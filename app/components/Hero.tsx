1c7730afe64b31598d3f39f73584a27228d48047
Kirill Nstrov <kirillbrat26@yandex.ru>
2026-05-16 00:03:33 +0600

    change: pallua doctor

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -117,7 +117,7 @@ const Hero = () => {
                             {/* Бейдж на изображении */}
                             <div className={styles.imageBadge} style={{ top: '90.55%'}}>
                                 <i className="fas fa-user-doctor"></i>
-                                <span>{t('Доктор Паулл')}</span>
+                                <span>{t('Профессор Н.Паллуа')}</span>
                             </div>
                             
                             {/* Подсказка о перевороте */}
